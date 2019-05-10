de88846b4a6c89a22083831ed7f8bb8afda55444
Naimahtech <naimatoyewale@yahoo.com>
2019-05-10 17:41:23 +0100

    chore(update):api set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,13 @@ module.exports = {
     "env" : {
         "mocha" : true,
         "browser" : true,
+        "es6": true,
+        "mocha": true,
+        "node": true,
+        "browser": true
     },
     "rules":{
-        "comma-dangle" : 0
+        "comma-dangle" : 0,
+         "indent":["warn",4]
     }
 }
