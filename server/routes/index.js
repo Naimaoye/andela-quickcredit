a31bc4dbc10c9bfda3c7f13ffe5c29f506aef817
Naimahtech <naimatoyewale@yahoo.com>
2019-05-13 12:44:50 +0100

    (chore): updated route and repayment files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,54 @@
-import userController from '../controller/users_controller';
-import validator from '../middle_ware/validate';
-import authenticate from '../middle_ware/authenticate';
+import { Router } from 'express';
+import Usercontroller from '../controller/users_controller';
+import Loancontroller from '../controller/loans_controller';
+import Repaymentcontroller from '../controller/repayments_controller';
+import Validator from '../middle_ware/validate';
+import Authenticate from '../middle_ware/authenticate';
+import Authorize from '../middle_ware/authorize';
+//import middleware from '../middle_ware/middleware';
+
+const {
+  usersValidator,
+  loginValidator,
+  loanApprovalValidator,
+  loanValidator,
+  loanQueryValidator,
+  loanRepaymentValidator
+} = Validator
+
+
+const {
+    createUsers,
+    loginUser,
+    adminVerifyUser
+} = Usercontroller
 
 const route = (app) => {
-    app.get('/api/v1/users', userController.getUsers);
-    app.post('/api/v1/auth/signup', userController.createUsers);
-    app.post('/api/v1/auth/signin' , userController.loginUser);
+    app.get('/api/v1/users', Usercontroller.getUsers);
+
+    app.post('/api/v1/auth/signup',usersValidator,createUsers);
+
+    app.post('/api/v1/auth/signin',loginValidator, Usercontroller.loginUser);
+
+    app.post('/api/v1/loans',loanValidator, Loancontroller.applyForLoan);
+
+    app.post('/api/v1/loans/:id/repayment',loanRepaymentValidator ,Repaymentcontroller.postRepayment);
+
+    app.patch('/api/v1/users/:email/verify' ,Usercontroller.adminVerifyUser);
+
+    app.patch('/api/v1/loans/:id', Loancontroller.loanApproval);
+
+    app.get('/api/v1/loans/:id/repayments', Repaymentcontroller.getRepaymentHistory);
+
+    app.get('/api/v1/loans', Loancontroller.getAllLoans);
+
+    app.get('/api/v1/loans?status=approved&repaid=false', Loancontroller.getAllLoans);
+
+    app.get('/api/v1/loans?status=approved&repaid=true', Loancontroller.getAllLoans);
+
+    app.get('/api/v1/loans/:id', Loancontroller.getSpecificLoan);
 };
+
 
 
 export default route;
