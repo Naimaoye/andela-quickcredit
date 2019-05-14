7f32702d9351aa219ccbafd41378d4ddbdc3c843
Naimahtech <naimatoyewale@yahoo.com>
2019-05-14 09:20:48 +0100

    (chore): added users model: endpoints

--- server/models/userss.js
+++ server/models/userss.js
@@ -1,10 +1,13 @@
+import Authenticate from '../middle_ware/authenticate';
+
+
 const users = [
     {
         id: 1,
         email: 'doyin@email.com',
         firstName: 'Ade',
         lastName: 'doyin',
-        password: 'password',
+        password: Authenticate.hashPassword('password'),
         address: 'cresent street',
         status: 'verified',
         isAdmin: false,
@@ -13,18 +16,18 @@ const users = [
     {
         id: 2,
         email: 'adebayo@ware.ng',
-        password: 'password',
+        password: Authenticate.hashPassword('password'),
         firstName: 'james',
         lastName: 'john',
         address: 'New york city',
-        status: 'verified',
+        status: 'not verified',
         isAdmin: false,
         token: '7qnrdw1wundefinedundefined'
     },
     {
         id: 3,
         email: 'adebayo@yahoo.com',
-        password: 'password',
+        password: Authenticate.hashPassword('password'),
         firstName: 'john',
         lastName: 'doe',
         address: 'Lekki phase I, Lagos',
@@ -32,18 +35,6 @@ const users = [
         isAdmin: true,
         token: '7qnrdw1wundefinedundefined'
     },
-    {
-        id: 4,
-        email: 'brianwhite@gmail.com',
-        password: 'password',
-        firstName: 'brian',
-        lastName: 'white',
-        address: 'Lekki phase I, Lagos',
-        status: 'verified',
-        isAdmin: true,
-        token: '7qnrdw1wundefinedundefined'
-    },
-
 ];
 
 export default users;
